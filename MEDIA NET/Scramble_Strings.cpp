b5452529d82e0740b00268481c5d2040a2a0ab15
Milind <milindsharmagithub@gmail.com>
2023-08-07 04:08:58 +0530

    Added Problems and Hints

--- MEDIA NET/Scramble_Strings.cpp
+++ MEDIA NET/Scramble_Strings.cpp
@@ -2,6 +2,47 @@
 using namespace std;
 using ll = long long int;
 
+
+class Solution {
+public:
+    bool isScramble(string s1, string s2) {
+        unordered_map <string,bool> dp;
+        function <bool(string,string)> dfs = [&](string a,string b){
+            if(a.size() != b.size()){
+                return false;
+            }
+            if(a == b){
+                return true;
+            }
+            if(a.size() == 1){
+                return false;
+            }
+            string key = a + " " + b;
+            if(dp.count(key)){
+                return dp[key];
+            }
+            bool ok = false;
+            int n = a.size();
+            for(int i=1;i<n;i++){
+                if(
+                    (
+                        dfs(a.substr(0,i),b.substr(0,i)) && dfs(a.substr(i),b.substr(i))
+                    ) 
+                    || 
+                    (
+                        dfs(a.substr(0,i),b.substr(n-i)) && dfs(a.substr(i),b.substr(0,n-i))
+                    )
+                ){
+                    ok = true;
+                    break;
+                }
+            }
+            return dp[key] = ok;
+        };
+        return dfs(s1,s2);
+    }
+};
+
 void solve(){
     
 }
